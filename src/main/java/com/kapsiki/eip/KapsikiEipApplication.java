cdc04fe9a75cff1c7fc01c9d2698aa5ecf565ca5
nsalifu <njidda.salifu@kouete.com>
2024-06-07 12:05:50 +0100

    kapsiki eip setup for di

---
+++ src/main/java/com/kapsiki/eip/KapsikiEipApplication.java
@@ -0,0 +1,21 @@
+package com.kapsiki.eip;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.openmrs.eip.app.config.AppConfig;
+import org.springframework.context.annotation.Import;
+
+
+@SpringBootApplication
+@Import(AppConfig.class)
+public class KapsikiEipApplication {
+	private static final Logger logger = LoggerFactory.getLogger(KapsikiEipApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("Starting the kapsiki EIP application...");
+		SpringApplication.run(KapsikiEipApplication.class, args);
+	}
+
+}
